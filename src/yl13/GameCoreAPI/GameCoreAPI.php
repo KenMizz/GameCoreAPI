abfd983519c133cdeb17a3d110bfe0ab20815232
Ken Mizz <mizzken16@gmail.com>
2018-08-05 19:21:47 +0800

    创建文件
    创建文件

---
+++ src/yl13/GameCoreAPI/GameCoreAPI.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *    ____                       ____                    _    ____ ___ 
+ * / ___| __ _ _ __ ___   ___ / ___|___  _ __ ___     / \  |  _ \_ _|
+ *| |  _ / _` | '_ ` _ \ / _ \ |   / _ \| '__/ _ \   / _ \ | |_) | | 
+ *| |_| | (_| | | | | | |  __/ |__| (_) | | |  __/  / ___ \|  __/| | 
+ * \____|\__,_|_| |_| |_|\___|\____\___/|_|  \___| /_/   \_\_|  |___|
+ * 
+ * GameCoreAPI是一个PocketMine的小游戏框架
+ * 游乐13制作
+ */
+
+namespace yl13\GameCoreAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\{TextFormat as TF, Config};
+
+use yl13\GameCoreAPI\EventListener;
+
+class GameCoreAPI extends PluginBase {
+
+    const VERSION = '1.0.0';
+    
+    private $ChatChannel = [];
+}
