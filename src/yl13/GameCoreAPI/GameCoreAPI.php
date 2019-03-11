d6e02faa5d13c37bc778264e3d4b2a135e48cac2
KenMizz <mizzken16@gmail.com>
2019-03-11 01:37:48 -0400

    加入AutoSaveTask，对经济系统做一些准备以及优化部分函数

--- src/yl13/GameCoreAPI/GameCoreAPI.php
+++ src/yl13/GameCoreAPI/GameCoreAPI.php
@@ -41,20 +41,20 @@ class GameCoreAPI extends PluginBase {
         )
     );
 
-    public function onEnable() {
+    public function onEnable() : void {
         $this->getLogger()->notice(TF::YELLOW."GameCoreAPI启动中...");
         $this->initPlugin();
     }
 
-    public function onLoad() {
+    public function onLoad() : void {
         self::$instance = $this;
     }
 
-    public function onDisable() {
+    public function onDisable() : void {
         $this->getLogger()->warning("GameCoreAPI已关闭");
     }
 
-    private function initPlugin() {
+    private function initPlugin() : void {
         $this->getLogger()->notice(TF::YELLOW."正在初始化...");
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         if(!is_file($this->getDataFolder().'config.yml')) {
@@ -63,16 +63,20 @@ class GameCoreAPI extends PluginBase {
         if(!is_dir($this->getDataFolder().'maps')) {
             @mkdir($this->getDataFolder().'maps');
         }
-        $this->initConfigure(new Config($this->getDataFolder().'config.yml', Config::YAML));
+        $this->initChatChannel(new Config($this->getDataFolder().'config.yml', Config::YAML));
+        $this->initEconomy(new Config($this->getDataFolder().'config.yml', Config::YAML));
         $this->api = new API($this);
         $this->getLogger()->notice(TF::GREEN."初始化成功!");
         $this->getLogger()->notice(TF::GREEN."当前版本:".TF::WHITE.self::VERSION);
     }
 
-    private function initConfigure(Config $config) {
-        //TODO: 新的Config校验
+    /**
+     * @param pocketmine\utils\Config $config
+     * 
+     * @return void
+     */
+    private function initChatChannel(Config $config) : void {
         $chatchannel = $config->get('chatchannel');
-        $economy = $config->get('economy');
         if(!is_bool($chatchannel['enabled'])) {
             $chatchannel['enabled'] = true;   
         }
@@ -91,8 +95,42 @@ class GameCoreAPI extends PluginBase {
             );
             $this->getLogger()->notice(TF::GREEN."已创建默认聊天频道:".TF::WHITE.$chatchannel['default']);
         }
-
         $this->Configure['chatchannel'] = $chatchannel;
+    }
+
+    /**
+     * @param pocketmine\utils\Config $config
+     * 
+     * @return void
+     */
+    private function initEconomy(Config $config) : void {
+        $economy = $config->get('economy');
+        if(!is_bool($economy['enabled'])) {
+            $economy['enabled'] = true;
+        }
+        if(!is_string($economy['money-name'])) {
+            $economy['money-name'] = 'Money';
+        }
+        if(!is_int($economy['money-max-limit'])) {
+            $economy['money-max-limit'] = PHP_INT_MAX;
+        }
+        if(!is_bool($economy['auto-save'])) {
+            $economy['auto-save'] = true;
+        }
+        if(!is_int($economy['auto-save-time'])) {
+            $economy['auto-save-time'] = 6000;
+        }
+        $this->Configure['economy'] = $economy;
+        if($this->getConfigure('economy', 'enabled') == true) {
+            if(!is_file($this->getDataFolder().'money.yml')) {
+                new Config($this->getDataFolder().'money.yml', Config::YAML);   
+            }
+            $config = new Config($this->getDataFolder().'money.yml', Config::YAML);
+            $this->playermoneyData = $config->getAll();
+            if($this->getConfigure('economy', 'auto-save') == true) {
+                $this->getScheduler()->scheduleRepeatingTask(new AutoSaveTask($this), $this->getConfigure('economy', 'auto-save-time'));
+            }
+        }
     }
 
     public static function getInstance() {
@@ -133,10 +171,11 @@ class GameCoreAPI extends PluginBase {
     }
 
     final public function getPlayerData(GameCoreAPI $plugin, Player $player) : ?Array {
-        if(isset($this->playerData[$player->getName()])) {
-            return $this->playerData[$player->getName()];
-        }
-        return null;
+        return $this->playerData[$player->getName()] ?? null;
+    }
+
+    final public function getPlayerMoneyData(GameCoreAPI $plugin, Player $player) : ?int {
+        return $this->playermoneyData[$player->getName()] ?? null;
     }
 
     final public function removePlayerData(GameCoreAPI $plugin, Player $player) : bool {
@@ -147,20 +186,8 @@ class GameCoreAPI extends PluginBase {
         return false;
     }
 
-    final public function getConfigure(String $value) {
-        switch($value) {
-
-            default:
-                return false;
-            break;
-
-            case 'chatchannel-enabled':
-                return $this->Configure['chatchannel']['enabled'];
-            break;
-
-            case 'chatchannel-default':
-                return $this->Configure['chatchannel']['default'];
-        }
+    final public function getConfigure($key, $value) {
+        return $this->Configure[$key][$value] ?? null;
     }
 
     final public function get(GameCoreAPI $plugin, String $type) {
@@ -195,7 +222,7 @@ class GameCoreAPI extends PluginBase {
         }
     }
 
-    final public function getGameNameById(int $gameid) : ?String {
+    final public function getGameNameById(int $gameid) : ?string {
         if(isset($this->registeredGames[$gameid])) {
             return $this->registeredGames[$gameid]['name'];
         }
