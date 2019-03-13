9c627b3a84927fe4ee6e67b7c79204107a6885a4
KenMizz <mizzken16@gmail.com>
2019-03-13 19:19:14 -0400

    经济API(WIP)

--- src/yl13/GameCoreAPI/api/economy.php
+++ src/yl13/GameCoreAPI/api/economy.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace yl13\GameCoreAPI\api;
 
 use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
 
 
 class economy extends API {
 
     private const FAILED_REASON = [
-        'player.not.exist' => '玩家不存在'
+        'API_DISABLED' => 'api没有被启用',
+        ''
     ];
 
     /**
      * @param int $gameid
      * @param pocketmine\Player $player
      * @param int $digit
+     * @description 设置玩家金钱
      * 
      * @return bool
      */
@@ -24,8 +27,36 @@ class economy extends API {
         $registeredGame = parent::getPlugin()->get(parent::getPlugin(), 'RGAME');
         if(isset($registeredGame[$gameid])) { //小游戏id有效
             if(parent::getPlugin()->getConfigure('economy', 'enabled')) { //API开启了
-                parent::getPlugin()->setPlayerData(parent::getPlugin(), $player, 'MONEY', $digit);
-                //parent::getPlugin()->getLogger()->notice();
+                if($digit <= parent::getPlugin()->getConfigure('economy', 'money-max-limit')) {
+                    parent::getPlugin()->setPlayerData(parent::getPlugin(), $player, 'MONEY', $digit);
+                    parent::getPlugin()->getLogger()->notice(TF::GREEN."小游戏 ".TF::WHITE.parent::getPlugin()->getGameNameById($gameid).TF::GREEN." 设置玩家金钱".TF::GREEN."成功");
+                    return true;
+                }
+                parent::getPlugin()->getLogger()->warning("小游戏 ".TF::WHITE.parent::getPlugin()->getGameNameById($gameid).TF::YELLOW."设置玩家金钱失败，原因:".TF::WHITE.self::FAILED_REASON['API_DISABLED']);
+            }
+            parent::getPlugin()->getLogger()->warning("小游戏 ".TF::WHITE.parent::getPlugin()->getGameNameById($gameid).TF::YELLOW."设置玩家金钱失败，原因:".TF::WHITE.self::FAILED_REASON['API_DISABLED']);
+            return false;
+        }
+        parent::getPlugin()->getLogger()->warning("游戏id:".TF::WHITE.$gameid.TF::YELLOW."设置玩家金钱".TF::YELLOW."失败，原因:".TF::WHITE.self::FAILED_REASON['GAMEID_NOT_REGISTERED']);
+        return false;
+    }
+
+    /**
+     * @param int $gameid
+     * @param pocketmine\Player $player
+     * @param int $digit
+     * @description: 增加玩家金钱
+     * 
+     * @return bool
+     */
+    final public function addMoney(int $gameid, Player $player, int $digit) : bool {
+        $registeredGame = parent::getPlugin()->get(parent::getPlugin(), 'RGAME');
+        if(isset($registeredGame[$gameid])) { //小游戏id有效
+            if(parent::getPlugin()->getConfigure('economy', 'enabled')) {
+                $playerData = parent::getPlugin()->getPlayerMoneyData(parent::getPlugin(), $player);
+                if(!$playerData['money'] + $digit >= parent::getPlugin()->getConfigure('economy', 'money-max-limit')) {
+                    //parent::getPlugin()-
+                }
             }
         }
     }
