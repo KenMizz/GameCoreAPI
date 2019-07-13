c01536ed086a2a0a920e89af317ca46d7153d1e8
KenMizz <mizzken16@gmail.com>
2019-07-13 21:45:27 +0800

    inital commit

---
+++ src/yl14/GameCoreAPI/GameCoreAPI.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace yl14\GameCoreAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class GameCoreAPI extends PluginBase {
+
+    const VERSION = "2.0.0";
+
+    public function onEnable() {
+        
+    }
+}
