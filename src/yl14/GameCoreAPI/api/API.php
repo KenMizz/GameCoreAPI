d7ac0c64d21f1bf3fefa50ff8cd0a0d43009db70
KenMizz <mizzken16@gmail.com>
2019-11-09 14:35:17 -0500

    ChatChannel

--- src/yl14/GameCoreAPI/api/API.php
+++ src/yl14/GameCoreAPI/api/API.php
@@ -11,13 +11,19 @@ class API {
     private $plugin;
 
     private $gamecore;
+    private $chatchannel;
 
     public function __construct(GameCoreAPI $plugin) {
         $this->plugin = $plugin;
         $this->gamecore = new GameCore($this->plugin);
+        $this->chatchannel = new ChatChannel($this->plugin);
     }
 
     public function getGameCore() : Gamecore {
         return $this->gamecore;
+    }
+
+    public function getChatChannel() : ChatChannel {
+        return $this->chatchannel;
     }
 }
---
+++ src/yl14/GameCoreAPI/api/ChatChannel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace yl14\GameCoreAPI\api;
+
+use yl14\GameCoreAPI\{
+    GameCoreAPI, utils\TextContainer
+};
+
+class ChatChannel {
+
+    private $plugin;
+    
+    private $channels = [];
+
+    const USAGE = [
+        'usage.chatchannel.create' => 'GREEN小游戏 WHITE %gamename% GREEN创建聊天频道 WHITE %channelname% 成功'
+    ];
+
+    public function __construct(GameCoreAPI $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * 创建聊天频道
+     * 
+     * @method bool create(string $gameid, string $channelname, string $chatformat = "")
+     * 
+     * @param String $gameid 小游戏id
+     * @param String $channelname 频道名
+     * @param String $chatformat 聊天格式
+     * 
+     * @return Boolean
+     * 
+     * Note: 聊天格式可以调整玩家在聊天时输出的内容，GameCoreAPI提供以下格式
+     * message: 玩家发送的信息
+     * playername: 玩家名
+     * 例: $chatformat = "[playername]: message"
+     * 这样的话在游戏内输出的内容就会是[XXX]: 你好！
+     */
+    public function create(string $gameid, string $channelname, string $chatformat = "") : bool {
+        $isIdValid = $this->plugin->getAPI()->getGameCore()->isIdValid($this->plugin, $gameid);
+        if($isIdValid) {
+            if(!isset($this->channels[$gameid])) {
+                $this->channels[$gameid] = array();
+            }
+            if(!isset($this->channels[$gameid][$channelname])) {
+                $this->channels[$gameid][$channelname] = array(
+                    'name' => $channelname,
+                    'players' => array(),
+                    'mute' => false,
+                    'chatformat' => $chatformat
+                );
+                $usage = str_replace(['%gamename%', '%channelname'], [$this->plugin->getAPI()->getGameCore()->getGameNameById($gameid), $channelname], self::USAGE['usage.chatchannel.create']);
+                $this->plugin->getLogger()->notice((new TextContainer($usage))->convertColor());
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
