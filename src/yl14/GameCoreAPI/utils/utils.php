05b533855079b8d01316e01ec2a7e19921829766
KenMizz <mizzken16@gmail.com>
2020-02-10 12:17:01 -0500

    utils shouldn't be a class

--- src/yl14/GameCoreAPI/utils/utils.php
+++ src/yl14/GameCoreAPI/utils/utils.php
@@ -2,9 +2,7 @@
 
 namespace yl14\GameCoreAPI\utils;
 
-class utils {
 
-    static public function generateENum(int $digit = 6) : string{
-        return substr(sha1(mt_rand()),17, $digit);
-    }
+function generateENum(int $digit = 6) : string{
+    return substr(sha1(mt_rand()),17, $digit);
 }
