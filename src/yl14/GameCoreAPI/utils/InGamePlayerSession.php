4185b95c750a847de9dc7182e815ddfa4d9df8c6
KenMizz <mizzken16@gmail.com>
2020-02-12 15:02:30 -0500

    rewrite InGamePlayerSession

---
+++ src/yl14/GameCoreAPI/utils/InGamePlayerSession.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace yl14\GameCoreAPI\utils;
+
+use pocketmine\Player;
+
+use yl14\GameCoreAPI\GameCoreAPI;
+
+class InGamePlayerSession {
+
+    private static $players = [];
+
+    static public function addPlayer(CustomPlayer $player) {
+        if(!isset($this->players[$player->getPlayer()->getName()])) {
+            self::$players[$player->getPlayer()->getName()] = $player;
+        }
+    }
+
+    static public function removePlayer(CustomPlayer $player) {
+        if(isset(self::$players[$player->getPlayer()->getName()])) {
+            unset(self::$players[$player->getPlayer()->getName()]);
+        }
+    }
+
+    static public function &getPlayer(GameCoreAPI $plugin, Player $player) {
+        return self::$players[$player->getName()] ?? false;
+    }
+}
