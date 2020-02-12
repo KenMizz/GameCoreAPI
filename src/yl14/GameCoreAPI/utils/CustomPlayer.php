8dc837a70b7e5e0f7356849c62359a93c7e198e5
KenMizz <mizzken16@gmail.com>
2020-02-12 15:02:17 -0500

    rewrite CustomPlayer

---
+++ src/yl14/GameCoreAPI/utils/CustomPlayer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace yl14\GameCoreAPI\utils;
+
+use pocketmine\Player;
+
+use yl14\GameCoreAPI\GameCoreAPI;
+
+class CustomPlayer {
+
+    private $plugin;
+
+    private $chatChannel = null;
+
+    private $player = null;
+
+    public function __construct(GameCoreAPI $plugin, Player $player) {
+        $this->plugin = $plugin;
+        $this->player = $player;
+    }
+
+    public function getPlayer() : Player {
+        return $this->player;
+    }
+    
+    public function setChatChannel(ChatChannel &$channel) {
+        $this->chatChannel = $channel;
+    }
+
+    public function &getChatChannel() : \yl14\GameCoreAPI\utils\ChatChannel {
+        return $this->chatChannel;
+    } 
+}
